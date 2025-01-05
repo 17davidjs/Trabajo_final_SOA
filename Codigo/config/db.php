5136d9fad55eef97ecae949f45303c7a1e28e5f0
Themrc63 <mikelrc63@gmail.com>
2025-01-05 19:45:55 +0100

    Update db.php

--- Codigo/config/db.php
+++ Codigo/config/db.php
@@ -3,15 +3,25 @@
     // ARCHIVO AISLADO PARA REQUERIRLO SIEMPRE EN LOS DEMÁS ARCHIVOS Y NO TENER QUE REPETIRLO TODO EL TIEMPO
 
     $host = "localhost";
-    $db = "soa_final";           // nombre de la base de datos
-    $user = "root";         // usuario de MySQL
-    $pass = "";         // contraseña de MySQL
+    $db = "soa_final";          // nombre de la base de datos
+    $user = "root";             // usuario de MySQL
+    $pass = "";                 // contraseña de MySQL
 
     $conn = new mysqli($host, $user, $pass, $db);
 
-        // Comprobar la conexión
+    // Comprobar la conexión
+    try
+    {
+        $conn = new mysqli($host, $user, $pass, $db);
         if ($conn->connect_error)
         {
-            die("Conexión fallida: " . $conn->connect_error);
+            throw new Exception("Conexión fallida: " . $conn->connect_error);
         }
+    }
+    
+    catch (Exception $e)
+    {
+        echo $e->getMessage();
+        exit;
+    }
 ?>
