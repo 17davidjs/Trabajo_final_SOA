b6ca07aac25f9848afa7cf4006e9973718292d7c
Alejandra García Martín <148661409+alebigfoot@users.noreply.github.com>
2025-01-07 15:03:25 +0100

    Update soa_final.sql

--- Base_datosSQL/soa_final.sql
+++ Base_datosSQL/soa_final.sql
@@ -127,7 +127,6 @@ CREATE TABLE `formacion` (
   `id` int(11) NOT NULL,
   `titulo` varchar(100) NOT NULL,
   `centro` varchar(255) NOT NULL,
-  `fecha_inicio` date NOT NULL,
   `fecha_fin` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
